4b432fe6e09f48679606a2e11eb56075fc596d39
Pavel Mironov <pashamironow@gmail.com>
2021-02-09 19:37:40 +0300

    add: vuestic-ui

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+//@ts-ignore
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(VuesticPlugin)
+app.use(router)
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <va-input type="text" v-model="value"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Options({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  value = ''
+}
 </script>
